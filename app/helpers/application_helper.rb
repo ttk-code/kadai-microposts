71b0db6dddd80f86a5b77d1156b2895572ac8693
EC2 Default User <ec2-user@ip-172-31-95-242.ec2.internal>
2020-03-31 18:13:28 +0000

    post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
-    include SessionsHelper
     
-    private
-
-  def require_user_logged_in
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
+    
+   
 end
